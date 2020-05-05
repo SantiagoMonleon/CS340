0d5acba27cbe4de286387bc3a5a31b4446bc99e6
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-05-04 20:36:59 -0700

    Adding table

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,9 +1,5 @@
 
 var calendar = document.getElementById("calendar")
 
-calendar.onload = function(){
-    alert("calendar loaded");
-    var test = document.createElement("h1");
-    test.textContent = "Load succesfull";
-    calendar.appendChild(test);
+function load_calendar(){
 };
