ab2f134bc6d98290ec3293e53b10a5494cc8911e
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-06-02 22:57:09 -0700

    removed dbcon

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_monleons',
-  password        : 'Graduate#2018',
-  database        : 'cs340_monleons'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 
 module.exports.pool = pool;
